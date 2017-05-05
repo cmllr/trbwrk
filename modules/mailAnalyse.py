56db066de0876d57b0062863c31f87aacc5eef30
chrism <chrism@rhrk.uni-kl.de>
2017-05-05 10:27:46 +0200

    + Extract mail infos

--- modules/mailAnalyse.py
+++ modules/mailAnalyse.py
@@ -1,61 +1,54 @@
 import imaplib
 import email
+from email.header import decode_header
 
-class Mail:
+class Sender(object):
+    Name = ""
+    Email = ""
+
+    def __init__(self,pRaw):
+        result = email.utils.parseaddr(pRaw)
+        self.Name = result[0]
+        self.Email = result[1]
+    def __str__(self):
+        name = ["-",self.Name][self.Name != ""]
+        return "{0} <{1}>".format(name,self.Email)
+
+class Mail(object):
     Subject=""
     Receiver=[],
-    Sender=""
+    Sender=None
     Attachments={}
-    def __init__(self,subject,receiver,sender,attachments):
-        self.Subject = subject
-        self.Receiver = receiver
-        self.Sender = sender
-        self.Attachments = attachments
-    def toString(self):
-        response = "{} => {}: \"{}\" {} attachments".format(self.Sender,self.Receiver,self.Subject,0) 
+
+    def __str__(self):
+        response = "{0} => {1}: \"{2}\" {3} attachments".format(self.Sender,self.Receiver,self.Subject,0) 
         return response
+        
 
-
-def getHeaderValue(headers,needle):
-    for i in range(0,len(headers)):
-        if (headers[i][0] == needle):
-            return headers[i][1]
-    return None
+def getHeader(message,needle):
+    response = ""
+    try:        
+        decode = email.header.decode_header(message[needle])[0]
+        response = unicode(decode[0])
+    except(UnicodeDecodeError):
+        response = message[needle]
+    return response
 
 
 def get_mails(server,port,emailaddr,password,folder):
     mail = imaplib.IMAP4_SSL(server,port)
     mail.login(emailaddr,password)
     mail.select(folder);
-    type, data = mail.search(None,'ALL')
-    mails = data[0].split();
-    first = int(mails[0]);
-    last = int(mails[-1]);
-    for i in range(last, first,-1):
-        type, mailBody =  mail.fetch(str(i),'(RFC822)')
-        for response_part in mailBody:
-            if isinstance(response_part,tuple):
-                msg = email.message_from_string(str(response_part[1]));
-                headers = msg.items();
-                attachments = {}
-                if msg.is_multipart():
-                    for part in msg.walk():
-                        mime = part.get_content_type()
-                        if part.get_content_maintype() == 'multipart':
-                            continue
-                        if part.get('Content-Disposition') is None:
-                            continue
-                        attachments[part.get_filename()] = part.get_payload(decode=True)
-                        if part.get_filename() is not None:
-                            fp = open("/run/media/chrism/497D-93DD/"+part.get_filename(), 'wb')
-                            fp.write(part.get_payload(decode=True))
-                            fp.close()
-                else:
-                    mime = msg.get_content_maintype()
-                    body = msg.get_payload(decode=True)
-                    #print(body)
-                
-                got = Mail(getHeaderValue(headers,"Subject"),getHeaderValue(headers,"To"),getHeaderValue(headers,"From"),{})
-                print(got.toString())
-        if i > 1:
-            break
+    
+    rv, data = mail.search(None, "ALL")    
+
+    for num in data[0].split():
+            rv, data = mail.fetch(num, '(RFC822)')
+
+            msg = email.message_from_string(data[0][1])
+            got = Mail()
+            got.Subject = getHeader(msg,"Subject")
+            got.Sender = Sender(getHeader(msg,"From"))
+            got.Receiver = getHeader(msg,"To")
+
+            print(got)
