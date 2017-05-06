1d2b57e99654eb07a02ea33dbac161e366a62988
cmllr <fury224@googlemail.com>
2017-05-06 13:11:09 +0200

    + Grab screenshot

--- modules/LinkAnalyse.py
+++ modules/LinkAnalyse.py
@@ -2,6 +2,9 @@ import re
 import whois
 import dns.resolver
 import urlparse
+import time
+import random
+import string
 
 class Hyperlink(object):
     Url=""
@@ -9,12 +12,13 @@ class Hyperlink(object):
     Whois={}
     Headers= {}
     Addresses=[]
+    Screenshot=""
 
     def __str__(self):
         if (self.Whois.name_servers == None):
-            return "{0} (HTTP {1}, {2}, {3})".format(self.Url,self.Status,self.Headers["Server"],self.Addresses);
+            return "{0} (HTTP {1} {4}, {2}, {3})".format(self.Url,self.Status,self.Headers["Server"],self.Addresses,self.Screenshot);
         else:
-            return "{0} (HTTP {1}, {7}, {8}, Location: {2}, {3} (*{4}), {5} ({6})".format(self.Url,self.Status,self.Whois.city,self.Whois.country,self.Whois.creation_date,self.Whois.name,self.Whois.emails,self.Headers["Server"],self.Addresses);
+            return "{0} (HTTP {1}, {7}, {8}, {9}, Location: {2}, {3} (*{4}), {5} ({6})".format(self.Url,self.Status,self.Whois.city,self.Whois.country,self.Whois.creation_date,self.Whois.name,self.Whois.emails,self.Headers["Server"],self.Addresses,self.Screenshot);
 
     __repr__ = __str__
 
@@ -52,11 +56,20 @@ def getIP(hostname):
             pass
 
     return addresses
-    
+
+def getScreen(url):
+    from selenium import webdriver
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(1920, 1080) # set the window size that you need 
+    driver.get(url)
+    time.sleep(5)
+    name = "".join(random.choice(string.lowercase) for i in range(8))
+    driver.save_screenshot('/tmp/'+name+'.png')
+    driver.quit()
+    return '/tmp/'+name+'.png';
 
 def getLinks(body):
     links = []
-    
     regex = re.compile(r"https?://[^ ^\"^\'^<]+")
     for match in re.findall(regex, body.replace("\n","").replace("\r","")):
         href = match;
@@ -65,6 +78,7 @@ def getLinks(body):
             domain = urlparse.urlparse(href)
             hostname = domain.hostname;
             if links.count(url.Url) == 0:
+                url.Screenshot = getScreen(url.Url)
                 url.Whois = getWHOIS(hostname)
                 url.Addresses = getIP(hostname)
                 links.append(url)
